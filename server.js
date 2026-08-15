06b93945bae37d2e4cbb1b62f2260e27e786959b
YTG2315 <152232174+YTG2315@users.noreply.github.com>
2026-08-14 21:50:17 -0400

    Update server.js
    Signed-off-by: YTG2315 <152232174+YTG2315@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 'use strict';
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const bcrypt = require('bcrypt');
-const app         = express();
+const express    = require('express');
+const bodyParser = require('body-parser');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const bcrypt     = require('bcrypt');
+const app        = express();
+
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
 fccTesting(app);
 const saltRounds = 12;
-const myPlaintextPassword = 'sUperpassw0rd!';
+const myPlaintextPassword = 'sUp3rpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
@@ -22,39 +27,9 @@ bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
 
 //START_SYNC
 
-
-
 //END_SYNC
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log("Listening on port:", PORT)
+  console.log(`Listening on port ${PORT}`);
 });
