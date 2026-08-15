77c0a0316fc9e28bb11701221f1eda16b17c9c70
YTG2315 <152232174+YTG2315@users.noreply.github.com>
2026-08-14 21:54:58 -0400

    Update server.js
    Signed-off-by: YTG2315 <152232174+YTG2315@users.noreply.github.com>

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express    = require('express');
 const bodyParser = require('body-parser');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
-const bcrypt     = require('bcrypt');
+const bcrypt     = require('bcryptjs');
 const app        = express();
 
 app.get('/', (req, res) => {
